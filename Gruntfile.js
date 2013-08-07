5759917dbc9a2e854d9428229f7fd4776d036e1b
Rob Kinyon <rob.kinyon@gmail.com>
2013-08-07 15:46:06 -0400

    Added jshint and basic tasks. Fixes #2.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,25 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      'spelcheker': {
+        src: [
+          __filename,
+        ],
+      },
+    },
   });
 
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-express');
+
+  grunt.registerTask('lint', [
+    'jshint',
+  ]);
+
+  grunt.registerTask('test', [
+    'lint',
+  ]);
+
+  grunt.registerTask('default', ['test']);
 };
